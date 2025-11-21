89d02e4595b14749a48d8e539298bbe3775d977c
jorgu <jorgu@users.noreply.github.com>
2025-11-21 17:27:10 +0100

    testar startApp

--- linkbuilder.js
+++ linkbuilder.js
@@ -1,19 +1,17 @@
 'use strict'
 
+export function startApp () {
+    const PREFIX = getLink();
+    createLinks ()
+
+}
+
 function getLink() {
     if (window.location.protocol === 'file:') {
         return "javascript:window.open('file://///";
     } else {
         return "file://///";
     }
-}
-
-
-
-export function startApp () {
-    const PREFIX = getLink();
-    createLinks ()
-
 }
 
 
